07d3954c069c19b53da94ef3f1264844c2239880
EuclidesCurso <euclides.curso@gmail.com>
2024-05-29 19:05:25 -0300

    feat:new repositorio

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import "./App.css"
+import Square from "./styles/styled-component/components/Square";
+
+function App() {
+  return (
+    <body>
+      <Square />
+    </body>
+  );
+}
+
+export default App;
+
+
